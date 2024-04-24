a82c76529c5e0e82b6dd1215c362945bbcf2b732
Ferchox <fernandotriguero123@gmail.com>
2024-04-24 08:37:37 -0400

    github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/social-links-profile-main/",
   plugins: [react()],
-  base:"https://ferchox.github.io/react-social-links/"
-})
+});
